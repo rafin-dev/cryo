c16e37aa29afd7d068ec487d4c92b9e299f8044f
Rafin <149442473+rafin-dev@users.noreply.github.com>
2026-03-24 14:28:24 -0400

    Fixed merge issues

--- cryo/src/runtime/raylib_bindings.cpp
+++ cryo/src/runtime/raylib_bindings.cpp
@@ -4,35 +4,35 @@
 
 namespace cryo::runtime::raylib {
 
-	ExpressionResult rl_init_window(const std::vector<ExpressionResult>& params)
+	CryoValue rl_init_window(const std::vector<CryoValue>& params)
 	{
 		InitWindow(
-			std::get<int32_t>(params[0]), 
-			std::get<int32_t>(params[1]),
+			std::get<int64_t>(params[0]), 
+			std::get<int64_t>(params[1]),
 			std::get<std::string>(params[2]).c_str());
-		return ExpressionResult();
+		return CryoValue();
 	}
 
-	ExpressionResult rl_close_window(const std::vector<ExpressionResult>& params)
+	CryoValue rl_close_window(const std::vector<CryoValue>& params)
 	{
 		CloseWindow();
-		return ExpressionResult();
+		return CryoValue();
 	}
 
-	ExpressionResult rl_window_should_close(const std::vector<ExpressionResult>& params)
+	CryoValue rl_window_should_close(const std::vector<CryoValue>& params)
 	{
 		return WindowShouldClose();
 	}
 
-	ExpressionResult rl_begin_drawing(const std::vector<ExpressionResult>& params)
+	CryoValue rl_begin_drawing(const std::vector<CryoValue>& params)
 	{
 		BeginDrawing();
-		return ExpressionResult();
+		return CryoValue();
 	}
 
-	ExpressionResult rl_end_drawing(const std::vector<ExpressionResult>& params)
+	CryoValue rl_end_drawing(const std::vector<CryoValue>& params)
 	{
 		EndDrawing();
-		return ExpressionResult();
+		return CryoValue();
 	}
 }
--- cryo/src/runtime/raylib_bindings.h
+++ cryo/src/runtime/raylib_bindings.h
@@ -4,11 +4,11 @@
 
 namespace cryo::runtime::raylib {
 
-	ExpressionResult rl_init_window(const std::vector<ExpressionResult>& params);
-	ExpressionResult rl_close_window(const std::vector<ExpressionResult>& params);
+	CryoValue rl_init_window(const std::vector<CryoValue>& params);
+	CryoValue rl_close_window(const std::vector<CryoValue>& params);
 
-	ExpressionResult rl_window_should_close(const std::vector<ExpressionResult>& params);
+	CryoValue rl_window_should_close(const std::vector<CryoValue>& params);
 
-	ExpressionResult rl_begin_drawing(const std::vector<ExpressionResult>& params);
-	ExpressionResult rl_end_drawing(const std::vector<ExpressionResult>& params);
+	CryoValue rl_begin_drawing(const std::vector<CryoValue>& params);
+	CryoValue rl_end_drawing(const std::vector<CryoValue>& params);
 }
